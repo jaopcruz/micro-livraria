f3d7997809acb39b3b79b486fbe3d5c3e5228e09
jaopcruz <joaopcruz2407@gmail.com>
2025-11-30 22:26:11 -0400

    Tarefa prática #1 - Microservices

--- services/controller/index.js
+++ services/controller/index.js
@@ -8,9 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 /**
- * Retorna a lista de produtos da loja via InventoryService
+ * Retorna a lista de produtos
+ * Aceita tanto '/products' quanto '/api/products'
  */
-app.get('/products', (req, res, next) => {
+app.get(['/products', '/api/products'], (req, res, next) => {
     inventory.SearchAllProducts(null, (err, data) => {
         if (err) {
             console.error(err);
@@ -22,9 +23,25 @@ app.get('/products', (req, res, next) => {
 });
 
 /**
- * Consulta o frete de envio no ShippingService
+ * Consulta um produto pelo ID
+ * Aceita tanto '/product/:id' quanto '/api/product/:id'
  */
-app.get('/shipping/:cep', (req, res, next) => {
+app.get(['/product/:id', '/api/product/:id'], (req, res, next) => {
+    inventory.SearchProductByID({ id: req.params.id }, (err, product) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'something failed :(' });
+        } else {
+            res.json(product);
+        }
+    });
+});
+
+/**
+ * Consulta o frete
+ * Aceita tanto '/shipping/:cep' quanto '/api/shipping/:cep'
+ */
+app.get(['/shipping/:cep', '/api/shipping/:cep'], (req, res, next) => {
     shipping.GetShippingRate(
         {
             cep: req.params.cep,
@@ -42,6 +59,10 @@ app.get('/shipping/:cep', (req, res, next) => {
         }
     );
 });
+
+// Se você já tiver implementado o Review (Tarefa 2), adicione as rotas dele aqui seguindo o mesmo padrão:
+// app.get(['/reviews/:id', '/api/reviews/:id'], ...);
+// app.post(['/reviews', '/api/reviews'], ...);
 
 /**
  * Inicia o router
